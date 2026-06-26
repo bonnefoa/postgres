3f1b6356b74de00b3e76b67b8c5709e4e2df7fec
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2026-06-26 11:06:16 +0200

    Add PGTRACE env to enable protocol tracing in libpq
    Add 2 env vars, PGTRACE and PGTRACEAPPEND. When PGTRACE is set, PQtrace
    will be set when connection options are processed. This allows to trace
    protocol messages like startup messages and BackendKeyData. This also
    allows enabling protocol tracing on any application using libpq.
    
    When a cancel connection is created with PQcancelCreate, we copy tracing
    fd and trace flags to allow the cancel requests to be traced. Since
    writes to the trace_file are now done line by line, we can share a fd
    between the source connection and the cancel connection.

--- src/interfaces/libpq/fe-cancel.c
+++ src/interfaces/libpq/fe-cancel.c
@@ -98,6 +98,12 @@ PQcancelCreate(PGconn *conn)
 	 */
 	cancelConn->cancelRequest = true;
 
+	/*
+	 * Copy trace fd and trace flags from the original connection
+	 */
+	cancelConn->Pfdebug = conn->Pfdebug;
+	cancelConn->traceFlags = conn->traceFlags;
+
 	if (!pqCopyPGconn(conn, cancelConn))
 		return (PGcancelConn *) cancelConn;
 
